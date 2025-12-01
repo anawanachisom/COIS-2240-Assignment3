199c54cb1ca160cf85649060d71a35ea7161f2af
anawanachisom <anawanachisom@gmail.com>
2025-11-30 19:53:31 -0500

    Task 1.5: Our Vehicle constructor has been refactired with the capitalize() helper method

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -8,19 +8,31 @@ public abstract class Vehicle {
     public enum VehicleStatus { Available, Held, Rented, UnderMaintenance, OutOfService }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
+//    	if (make == null || make.isEmpty())
+//    		this.make = null;
+//    	else
+//    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
+//    	
+//    	if (model == null || model.isEmpty())
+//    		this.model = null;
+//    	else
+//    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
     	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
-    	
+    	make = capitalize(make);        // Use helper method
+        model = capitalize(model);
         this.year = year;
         this.status = VehicleStatus.Available;
         this.licensePlate = null;
+    }
+    
+ // THIS PRIVATE HELPER METHOD HELPS US CAPITALIZE THE FIRST LETTER OF MODEL AND MAKE
+    private String capitalize(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+        String firstLetter = input.substring(0, 1).toUpperCase();
+        String rest = input.substring(1).toLowerCase();
+        return firstLetter + rest;
     }
 
     public Vehicle() {
